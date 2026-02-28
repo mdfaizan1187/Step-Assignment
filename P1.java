6c44947c6203e39e4189d8b8621b7ef7c0d6278e
Mohammed Faizan <mdfaizanansari1187@gmail.com>
2026-02-28 22:29:39 +0530

    Level 1

---
+++ P1.java
@@ -0,0 +1,9 @@
+//QUE 1. Write a program to calculate the age of a person in 2024, given their birth year.
+public class Level_One {
+    public static void main(String[] args) {
+        int birthyear = 2000;
+        int curryear = 2024;
+        int age = curryear - birthyear;
+        System.out.println("Harry's Age in 2024 is: " + age);
+    }
+}
