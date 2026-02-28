89bbd2dc661d222b358315073a1d2049bf812143
Mohammed Faizan <mdfaizanansari1187@gmail.com>
2026-02-28 23:49:14 +0530

    Level 3

--- pr1.java
+++ pr1.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Codes_Level_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float celsius, fahrenheit;
+        float celsius;
+        float fahrenheit;
         System.out.println("Enter the temperature in Celsius: ");
         celsius = sc.nextFloat();
         fahrenheit = (celsius * 9/5) + 32;
